0b7c51ab55346db5c40bc5f0e878176e5ffba8c3
amachar <amachar.aravindasamy@gmail.com>
2017-11-05 12:53:50 +0530

    added print msg, whenever  request comes, the msg will get printed on console

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ http.createServer(function(req,res)
 	{
 		res.writeHead(200,{'Content-Type':'text/plain'});
 		res.end("hello world!");
+		console.log("1 request processed")
 	}
 ).listen(8084);
 console.log("server is running")
